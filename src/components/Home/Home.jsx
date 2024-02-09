cc1f29060f13826b8be0918952b12e3bfa8a6c90
Siddhant <me@siddhantgupta.in>
2024-02-10 02:29:18 +0530

    first commit

---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import './Home.module.css';
+
+const HOME = () =>{
+    return(
+        <>
+                                    HOME
+        </>
+    );
+}
+export default HOME;
---
+++ src/components/Pages/AboutMe/AboutMe.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+import './AboutMe.module.css';
+
+const ABOUT = () =>{
+    return(
+        <>
+                About
+
+
+        </>
+    );
+}
+export default ABOUT;
