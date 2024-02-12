09e2c2da448aa4b8cef770508e8051a49bc87892
Siddhant <me@siddhantgupta.in>
2024-02-12 19:26:42 +0530

    Updated the navbar with CSS

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,10 +1,35 @@
 import React from "react";
 import style from "./Navbar.module.css";
 
+import CloseIcon from '@mui/icons-material/Close';
+import MenuIcon from '@mui/icons-material/Menu';
+
+import KnmLOGO from "../../img/KNM_LOGO.png";
 const NAVBAR = () =>{
     return (
         <>
-            <h1>Navbar</h1>
+           <header className={style.navbar}>
+                <div className={style.logo}>
+                        <img src={KnmLOGO} alt="knmStudio" height={90}  />
+                </div>
+                <ul className={style.links}>
+                    <li className={style.link}>Home</li>
+                    <li className={style.link}>About Us</li>
+                    <li className={style.link}>Services</li>
+                    <li className={style.link}>Our Work</li>
+                    <li className={style.link}>Blogs</li>
+                    <li className={style.link}>Contact Us</li>
+                </ul>
+                <div className={style.actionBtn}>
+                    Start a Project
+                </div>
+
+                <div className={style.toggleNav}>
+                      {/* <CloseIcon />  */}
+                      <MenuIcon />
+                </div>
+
+           </header>
         </>
     );
 }
