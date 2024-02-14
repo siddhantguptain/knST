e2124c4f9013fd3393704a88eb393a4418f8bf6b
Siddhant <me@siddhantgupta.in>
2024-02-14 16:25:52 +0530

    Updated the Footer

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,33 @@
+import style from "./Footer.module.css";
+
+
+const FOOTER = () =>{
+    return (
+        <>
+            <footer className={style.footerCon} >
+                <div className={style.container}>
+                    <div className={style.callForAction}>
+                         LET'S BUILD SOMETHING <br/> AMAZING TOGETHER
+                    </div>
+                    <div className={style.contact}>
+                    <h3 className={style.footerHeading}>Contact</h3>
+                         <span>T : +91 84596 39610</span><br />
+                         <span>E : business@knmstudio.in</span> 
+
+                          
+                    </div>
+                    <div className={style.address}>
+                        <h3 className={style.footerHeading}>Head Office </h3>
+                    101, Atheshri Apartment,<br />Baner, Pune, India <br />411045
+                    </div>
+                </div>
+                <div className={style.line} />
+                <div className={style.copyRight} >
+                         KNM Studio © 2024 Designed by Siddhant Gupta
+                </div>
+                
+            </footer>
+        </>
+    );
+}
+export default FOOTER;
