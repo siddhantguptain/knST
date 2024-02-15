bd30970275fdff8de12e84bcee41376557d1ffc4
Siddhant <me@siddhantgupta.in>
2024-02-15 12:31:12 +0530

    Added the HeadTag

--- src/components/Pages/OurWork/OurWork.jsx
+++ src/components/Pages/OurWork/OurWork.jsx
@@ -3,7 +3,23 @@ import style from "./OurWork.module.css";
 const OurWork = () =>{
     return(
         <>
-            <h1>Our Work</h1>
+          <div className={style.mainContainer}>
+            <div className={style.subContaierOne}>
+                <div className={style.title}>
+                    <h1>OUR WORK<br /> <span className={style.titleHead}>EXPLORE → VISUALISE → CREATE</span></h1>
+                    
+                </div>
+                     <div className={style.callForAction}></div>
+            </div>
+            {/* Second Container */}
+             <div className={style.subContaierSecond}>
+                <div className={style.subTitle}>SOME OF OUR BEST WORKS
+                </div>   
+                <div className={style.subHead}>ADVERTISEMENT || EVENTS || CORPORATE VIDEOS || INFORMATIVE VIDEOS || E-COMMERCE
+                </div>   
+             </div>   
+
+         </div>   
         </>
     );
 }
