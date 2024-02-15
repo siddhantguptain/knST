e43822d1ff13da9ac65b691f1655b46d0fdce59c
Siddhant <me@siddhantgupta.in>
2024-02-15 16:54:54 +0530

    Initalizing Contact  Page

---
+++ src/components/Pages/ContactUs/ContactUs.jsx
@@ -0,0 +1,12 @@
+import style from './ContactUs.module.css';
+
+const ContactUs = () =>{
+    return (
+        <>
+            <div className={style.container}>
+                ContactUs
+            </div>
+        </>
+    );
+}
+export default ContactUs;
