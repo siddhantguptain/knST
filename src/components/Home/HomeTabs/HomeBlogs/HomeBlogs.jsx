7f62cac9e1ef5bc0841dffd15f217439fc289de6
Siddhant <me@siddhantgupta.in>
2024-02-15 18:26:10 +0530

    Initializing HOme Blog Tab

---
+++ src/components/Home/HomeTabs/HomeBlogs/HomeBlogs.jsx
@@ -0,0 +1,12 @@
+import style from './HomeBlogs.module.css';
+
+const HomeBlogs =() =>{
+    return(
+        <>  
+            <div className={style.container}>
+                HomeBlogs
+            </div>
+        </>
+    );
+}
+export default HomeBlogs;
