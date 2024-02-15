a3f7e47a0814cf102c89c824a1d8e1a9494d3949
Siddhant <me@siddhantgupta.in>
2024-02-15 17:52:17 +0530

    Added the Navbar Links

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css';
+import { Routes,Route } from "react-router-dom";
 import NAVBAR from './components/Navbar/Navbar';
-import AboutMe from './components/Pages/AboutMe/AboutMe';
+import Home from './components/Home/Home'
+import AboutUs from './components/Pages/AboutUs/AboutUs';
+import Services from './components/Pages/Services/Services';
+import OurWork from './components/Pages/OurWork/OurWork';
+import Blogs from './components/Pages/Blogs/Blogs';
+import ContactUs from './components/Pages/ContactUs/ContactUs';
+import FOOTER from './components/Footer/Footer';
 
 function App() {
   return (
     <div className="App"> 
           <NAVBAR />
-          <AboutMe />
+            <Routes>
+              <Route path="/" element={<Home />} />
+              <Route path="/about-us" element={<AboutUs />}  />
+              <Route path="/services" element={<Services />}  />
+              <Route path="/our-work" element={<OurWork />}  />
+              <Route path="/blogs" element={<Blogs />}  />
+              <Route path="/contact-us" element={<ContactUs />}  />
+            </Routes>
+          <FOOTER />
     </div>
   );
 }
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import style from "./Navbar.module.css";
 
+import { Link } from "react-router-dom";
 import CloseIcon from '@mui/icons-material/Close';
 import MenuIcon from '@mui/icons-material/Menu';
 
@@ -8,18 +9,18 @@ import KnmLOGO from "../../img/KNM_LOGO.png";
 const NAVBAR = () =>{
     return (
         <>
-           <header >
+           <header className={style.nav}>
             <div className={style.navbar}>
                 <div className={style.logo}>
                         <img src={KnmLOGO} alt="knmStudio" height={90}  />
                 </div>
                 <ul className={style.links}>
-                    <li className={style.link}>Home</li>
-                    <li className={style.link}>About Us</li>
-                    <li className={style.link}>Services</li>
-                    <li className={style.link}>Our Work</li>
-                    <li className={style.link}>Blogs</li>
-                    <li className={style.link}>Contact Us</li>
+                    <li className={style.link}><Link to="/">Home</Link></li>
+                    <li className={style.link}><Link to="/about-us">About Us</Link></li>
+                    <li className={style.link}><Link to="/services">Services</Link></li>
+                    <li className={style.link}><Link to="/our-work">Our Work</Link></li>
+                    <li className={style.link}><Link to="/blogs">Blogs</Link></li>
+                    <li className={style.link}><Link to="/contact-us">Contact Us</Link></li>
                 </ul>
                 <div className={style.actionBtn}>
                     Start a Project
