303a72403f56aabc00feea15c740c6ed8b81a382
Siddhant <me@siddhantgupta.in>
2024-02-15 16:50:37 +0530

    Initalizing Blog Page

---
+++ src/components/Pages/Blogs/Blogs.jsx
@@ -0,0 +1,12 @@
+import style from './Blogs.module.css';
+
+const Blogs = () =>{
+    return (
+        <>
+            <div className={style.container}>
+                Blogs
+            </div>
+        </>
+    );
+}
+export default Blogs
