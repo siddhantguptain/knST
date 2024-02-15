7666be13fd52dfb1cad46bd5bcf3b7706d570e2a
Siddhant <me@siddhantgupta.in>
2024-02-15 16:52:35 +0530

    Initalizing Services  Page

---
+++ src/components/Pages/Services/Services.jsx
@@ -0,0 +1,12 @@
+import style from './Services.module.css';
+
+const Services = () =>{
+    return (
+        <>
+            <div className={style.container}>
+                Services
+            </div>
+        </>
+    );
+}
+export default Services;
