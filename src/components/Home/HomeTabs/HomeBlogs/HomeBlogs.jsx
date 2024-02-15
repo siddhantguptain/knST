979ceed3cc0d84834210f466a4a8d52fe1c88741
Siddhant <me@siddhantgupta.in>
2024-02-15 18:37:26 +0530

    Initializing HOmeblog card and the hoem blog Heading

--- src/components/Home/HomeTabs/HomeBlogs/HomeBlogs.jsx
+++ src/components/Home/HomeTabs/HomeBlogs/HomeBlogs.jsx
@@ -1,10 +1,17 @@
 import style from './HomeBlogs.module.css';
 
+import HomeBlogsCard from './HomeBlogsCard/HomeBlogsCard';
 const HomeBlogs =() =>{
     return(
         <>  
             <div className={style.container}>
-                HomeBlogs
+                <div className={style.subTitle}>
+                    <h5>⦿ LATEST NEWS</h5>
+                </div>
+                <div className={style.title}>
+                    <h1>LATEST AND GREATEST POST</h1>
+                </div>
+                <HomeBlogsCard />
             </div>
         </>
     );
---
+++ src/components/Home/HomeTabs/HomeBlogs/HomeBlogsCard/HomeBlogsCard.jsx
@@ -0,0 +1,12 @@
+import style from './HomeBlogsCard.module.css';
+
+const HomeBlogsCard =() =>{
+    return(
+        <>  
+            <div className={style.container}>
+                HomeBlogCard
+            </div>
+        </>
+    );
+}
+export default HomeBlogsCard;
