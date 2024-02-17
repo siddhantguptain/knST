2c430d44addf48e81fc3050d357ceb347924f2b8
Siddhant <me@siddhantgupta.in>
2024-02-18 01:08:26 +0530

    Added the Icons and Text color to them

--- src/components/Home/HomeTabs/HomeService/HomeService.jsx
+++ src/components/Home/HomeTabs/HomeService/HomeService.jsx
@@ -2,6 +2,14 @@ import style from './HomeService.module.css';
 
 import ServiceCard from './ServiceCard/ServiceCard';
 
+import { PiLightbulbLight } from "react-icons/pi";
+import { PiVideoLight } from "react-icons/pi";
+import { CiVideoOn } from "react-icons/ci";
+import { CiShoppingCart } from "react-icons/ci";
+import { CiLocationOn } from "react-icons/ci";
+import { PiFileVideoLight } from "react-icons/pi";
+
+
 const HomeService =() =>{
     return(
         <>  
@@ -9,31 +17,37 @@ const HomeService =() =>{
                  <h3 className={style.title} >LET’S CHECK OUR SERVICES</h3>
                 <div className={style.serviceCard}>
                     <ServiceCard className={style.Card}
+                         icon={<PiLightbulbLight  size={90} />}     
                          ServiceTitle="BRAND DEVELOPMENT
                          " 
                          ServicePara="Trust us for crafting resonant brand identities through research, creative storytelling, and authentic narratives, setting you apart in India."
                     />
                     <ServiceCard className={style.Card}
+                         icon={<PiVideoLight    size={90} />}   
                          ServiceTitle="ADVERTISEMENT
                          " 
                          ServicePara="Crafting captivating ads that resonate with Indian market dynamics, our media production ignites action and delivers results, cutting through clutter."
                     />
                     <ServiceCard className={style.Card}
+                         icon={<CiVideoOn   size={90} />}   
                          ServiceTitle="CORPORATE VIDEOS
                          " 
                          ServicePara="Mastering corporate video production, we convey brand ethos through captivating visual narratives—leaving lasting impacts."
                     />
     
                     <ServiceCard className={style.Card}
+                         icon={<CiShoppingCart   size={90} />}   
                          ServiceTitle="E-COMMERCE
                          " 
                          ServicePara="Expertly crafting e-commerce shoots, breathing life into products, understanding Indian market aesthetics."
                     />
                     <ServiceCard className={style.Card}
+                         icon={<CiLocationOn   size={90} />}   
                          ServiceTitle="EVENTS
                          " 
                          ServicePara="Excelling in immortalizing emotions, we deliver event coverage that truly sets us apart from competitors."
                     /><ServiceCard className={style.Card}
+                         icon={<PiFileVideoLight   size={90} />}   
                     ServiceTitle="EXPLAINER VIDEO
                     " 
                     ServicePara="We craft engaging explainer videos, simplify concepts, promote products, and captivate audiences effectively."
--- src/components/Home/HomeTabs/HomeService/ServiceCard/ServiceCard.jsx
+++ src/components/Home/HomeTabs/HomeService/ServiceCard/ServiceCard.jsx
@@ -1,12 +1,13 @@
 import style from './ServiceCard.module.css';
-import { FaRegLightbulb } from "react-icons/fa";
+
 
 const ServicesCard = (props ) =>{
     return(
         <>  
             <div className={style.container}>
                 <div className={style.serviceIcon}>
-                <FaRegLightbulb size={90} />
+                {/* <FaRegLightbulb size={90} /> */}
+                {props.icon}
                 </div>
                 <div className={style.serciveTitle}>
                 {props.ServiceTitle}
