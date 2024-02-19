ac62229d4d000f2e88f4396642e01a26173b72ef
Siddhant <me@siddhantgupta.in>
2024-02-19 14:44:23 +0530

    Added the the Video Dispay in the home page

--- src/components/Home/HomeTabs/HomeVideoSlider/HomeVideoSlider.jsx
+++ src/components/Home/HomeTabs/HomeVideoSlider/HomeVideoSlider.jsx
@@ -5,6 +5,7 @@ import AOS from 'aos';
 import 'aos/dist/aos.css';
 
 import One8 from '../../../../video/One8.mp4'
+// import One8 from '../../../../video/ACTIMAX - BRAND MASTER.mp4'
 
 const HomeVideoSlider = () =>{
          useEffect(() =>{
@@ -13,7 +14,16 @@ const HomeVideoSlider = () =>{
     return(
         <>
              <div className={style.container}>
+                <div className={style.overlay}></div>
                <video src={One8} autoPlay muted loop/>
+               <div className={style.content}>
+               <div className={style.title}>
+                        <h1>ONE 8 - ATTITUDE</h1>
+                    </div>
+                    <div className={style.subTitle}>⦿ Advertisement</div>
+                
+                    <div className={style.para}>Embracing the bold attitude that comes after wearing the undergarments of One8 <br />Fearlessly conquer every challenge.</div>
+               </div>
              </div>
      
         </>
