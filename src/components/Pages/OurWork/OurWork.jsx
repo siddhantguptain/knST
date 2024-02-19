416f9ddbcddfed3e37feeec7292a842fbbd12edd
Siddhant <me@siddhantgupta.in>
2024-02-19 15:20:16 +0530

    Added Aniamtion

--- src/components/Pages/OurWork/OurWork.jsx
+++ src/components/Pages/OurWork/OurWork.jsx
@@ -1,12 +1,26 @@
+import React, {useEffect}   from "react";
 import style from "./OurWork.module.css";
 
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
 const OurWork = () =>{
+    useEffect(() =>{
+        AOS.init({duration:1400});
+    },[]);
     return(
         <>
           <div className={style.mainContainer}>
             <div className={style.subContaierOne}>
-                <div className={style.title}>
-                    <h1>OUR WORK<br /> <span className={style.titleHead}>EXPLORE → VISUALISE → CREATE</span></h1>
+                <div className={style.title} 
+                data-aos="zoom-in-right"
+                data-aos-offset="300"
+                data-aos-easing="ease-in-sine"
+                >
+                    <h1>OUR WORK<br /> 
+                    <span className={style.titleHead}>
+                        EXPLORE → VISUALISE → CREATE
+                    </span></h1>
                     
                 </div>
                      <div className={style.callForAction}></div>
