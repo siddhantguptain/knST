b22809ef24032fca90c9f84fe07167a0c14c0508
Siddhant <me@siddhantgupta.in>
2024-02-19 12:36:37 +0530

    Added the AOS Animation

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,13 +1,20 @@
-import React from "react";
+import React, {useEffect}  from "react";
 import style from'./Home.module.css';
+
+import AOS from 'aos';
+import 'aos/dist/aos.css';
 
 import HomeAbout from "./HomeTabs/HomeAbout/HomeAbout";
 import HomeService from "./HomeTabs/HomeService/HomeService";
+import HomeTestimonial from "./HomeTabs/HomeTestimonial/HomeTestimonial";
 import HomeBlogs from "./HomeTabs/HomeBlogs/HomeBlogs";
 
 import BrandQue from "../BrandQue/BrandQue";
 
 const Home = () =>{
+         useEffect(() =>{
+        AOS.init({duration:2000});
+    },[]);
     return(
         <>
             <div className={style.homeContainer}>
@@ -21,15 +28,15 @@ const Home = () =>{
                    <HomeService />
                    {/* Testimonial */}
                    <div className={style.queDiv}>
-                   <div className={style.subTitle}>
-                    <h5>⦿ OUR CLIENTS</h5>
-                </div>
-                <div className={style.title}>
-                    <h1>YOUR SUCCESSFUL, OUR REPUTATION</h1>
-                </div>
-                        <BrandQue />
+                             <div className={style.subTitle} data-aos="fade-right">
+                                 <h5>⦿ OUR CLIENTS</h5>
+                            </div>
+                            <div className={style.title}  data-aos="fade-right" >
+                                <h1>YOUR SUCCESSFUL, OUR REPUTATION</h1>
+                              </div>
+                              <div data-aos="zoom-in-down"><BrandQue /></div>
                    </div>
-                   
+                   <HomeTestimonial />
                    <HomeBlogs />
                 </div>
             </div>
