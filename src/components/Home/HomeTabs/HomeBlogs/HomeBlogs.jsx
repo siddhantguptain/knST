c444df3b6e3db387f33f9965812373d18e732b33
Siddhant <me@siddhantgupta.in>
2024-03-03 16:56:25 +0530

    updated the blog tab

--- src/components/Home/HomeTabs/HomeBlogs/HomeBlogs.jsx
+++ src/components/Home/HomeTabs/HomeBlogs/HomeBlogs.jsx
@@ -3,22 +3,42 @@ import style from './HomeBlogs.module.css';
 
 import AOS from 'aos';
 import 'aos/dist/aos.css';
+import BL1 from "../../../../img/BlogImg/BLOG 1 Digital Marketing Illustration.png"
+import BL2 from "../../../../img/BlogImg/Blog 2 VNU_M715_4.png"
+import BL3 from "../../../../img/BlogImg/BLOG 3 Influencer.png"
+
 
 import HomeBlogsCard from './HomeBlogsCard/HomeBlogsCard';
 const HomeBlogs =() =>{
     useEffect(() =>{
-        AOS.init({duration:2000});
+        AOS.init({duration:1000});
     },[]);
     return(
         <>  
-            <div className={style.container} data-aos="fade-right">
+            <div className={style.container} data-aos="fade-up">
                 <div className={style.subTitle}>
                     <h5>⦿ LATEST NEWS</h5>
                 </div>
-                <div className={style.title} data-aos="fade-right">
+                <div className={style.title} data-aos="fade-up">
                     <h1>LATEST AND GREATEST POST</h1>
                 </div>
-                <HomeBlogsCard />
+            <div className={style.subContainer} data-aos="fade-up">
+                    <HomeBlogsCard 
+                        src={   <img src={BL1} alt=">Why is marketing important for businesses?" height="300"/>} 
+                        tag="July 12, 2023 .. Marketing"
+                        title="Why is marketing important for businesses?"
+                    />
+                    <HomeBlogsCard 
+                        src={   <img src={BL2} alt=">Why is marketing important for businesses?" height="300"/>} 
+                        tag="July 18, 2023 .. Editorial"
+                        title="Power of story telling in content marketing."
+                    />
+                    <HomeBlogsCard 
+                        src={   <img src={BL3} alt=">Why is marketing important for businesses?" height="300"/>} 
+                        tag="July 22, 2022 .. Educational"
+                        title="Growth of influencer marketing"
+                    />
+            </div>
             </div>
         </>
     );
--- src/components/Home/HomeTabs/HomeBlogs/HomeBlogsCard/HomeBlogsCard.jsx
+++ src/components/Home/HomeTabs/HomeBlogs/HomeBlogsCard/HomeBlogsCard.jsx
@@ -1,10 +1,16 @@
 import style from './HomeBlogsCard.module.css';
 
-const HomeBlogsCard =() =>{
+
+const HomeBlogsCard =(props) =>{
     return(
         <>  
             <div className={style.container}>
-                
+                <div className={style.imgDIV}>
+                  {props.src}
+                </div>
+                <div className={style.tag}>{props.tag}</div>
+                <div className={style.title}>{props.title}</div>
+                <div className={style.readBtn}>Read Artice</div>
             </div>
         </>
     );
