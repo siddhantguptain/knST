bbb442c598ae0c9a1bc372fbeb389ce1e970649b
Siddhant <me@siddhantgupta.in>
2024-03-04 10:59:14 +0530

    Updated the About Tab

--- src/components/Home/HomeTabs/HomeAbout/HomeAbout.jsx
+++ src/components/Home/HomeTabs/HomeAbout/HomeAbout.jsx
@@ -4,7 +4,39 @@ const HomeAbout =() =>{
     return(
         <>  
             <div className={style.container}>
-                -
+              <div className={style.subContainer} data-aos="fade-right">
+              <div className={style.aboutSubTitle}  >
+                    <h5>⦿ ABOUT US</h5>
+                </div>
+                <div className={style.aboutTitle} data-aos="fade-up">
+                    WHO WE ARE
+                </div>
+                {/* Main */}
+                <div className={style.parameters}>
+
+                </div>
+
+              </div>
+              <div className={style.subContainer} data-aos="fade-left">
+                <div className={style.subContainerHead}>
+                     HELLO WE, KNM GROUP
+                </div>
+                <div className={style.subContainerPara}>
+                    Founded in 2020, KNM Studio is a top video production company in Pune. Embrace digital connectivity and elevate your business with our cutting-edge tools and techniques for a competitive edge and remarkable success. Welcome to the world of captivating visual storytelling.
+                </div>    
+                <div className={style.subContainerTag}>
+                    Sparsh Agrawal <br/>
+                    
+                </div>  
+                <div className={style.subContainerTagRole} >Founder</div>
+                <div className={style.subContainerExp}>
+                    <div className={style.subContainerExpNum}>
+                        38
+                    </div>
+                    <div className={style.subContainerExpText}>MONTHS OF <br />
+DIGITAL EXPERIENCE</div>
+                </div>
+              </div>              
             </div>
         </>
     );
