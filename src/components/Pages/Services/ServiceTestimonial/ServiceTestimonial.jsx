b1533da4fad9118a4b7abe690b04e280d0ded9c6
Siddhant <me@siddhantgupta.in>
2024-03-13 16:58:21 +0530

    Added the Service Tabs

---
+++ src/components/Pages/Services/ServiceTestimonial/ServiceTestimonial.jsx
@@ -0,0 +1,77 @@
+import { useEffect } from "react";
+import style from './ServiceTestimonial.module.css';
+
+
+import Slider from "react-slick";
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import AOS from 'aos';
+import 'aos/dist/aos.css';
+
+import HomeTestimonialCard from "../../../Home/HomeTabs/HomeTestimonial/HomeTestimonialCard/HomeTestimonialCard";
+
+import { FaCircle } from "react-icons/fa";
+
+const HomeTestimonial =() =>{
+
+    useEffect(() =>{
+        AOS.init({duration:1000});
+    },[]);
+
+
+    const TestimonialData = [
+        {
+            key:1,
+            text:"I have been working with the digital marketing experts at KNM Studio for over a year now, and I am extremely impressed with the results. Their strategies have significantly boosted our online presence, and our sales have skyrocketed. The team is highly knowledgeable, responsive, and a pleasure to work with. I highly recommend KNM Studio to any business looking to elevate their online marketing efforts.",
+            userName:"Prajakta Gupta",
+            userRole:"Founder Cologne Spa"
+        },
+        {
+            key:2,
+            text:"KNM Studio's video production work is outstanding! Their creative team consistently delivers impactful videos that elevate our marketing efforts. Highly commendable!",
+                            userName:"Siddharth Gupta",
+                            userRole:"Marketing Head -  Spardha Learning"
+        },
+        {
+            key:3,
+            text:"   The content created by KNM Studio is exceptional! Their team consistently delivers captivating and high-quality content that engages our audience effectively. Highly recommended!",
+            userName:"Umar Farukh",
+            userRole:"Terminak Head Vivo"
+        }
+
+
+    ]
+    const settings = {
+        dots: true,
+        infinite: true,
+        speed: 500,
+        slidesToShow: 2,
+        slidesToScroll: 1
+      };
+    
+    return(
+        <>  
+            <div className={style.container} data-aos="fade-right">
+                <div className={style.title}>
+                <div> RELATIONSHIPS <b>BASED ON TRUST.</b></div>
+                </div>
+                
+                <div className={style.subContainer} data-aos="zoom-in-up">
+                <Slider {...settings}>
+                {TestimonialData.map((el) =>(
+                    <HomeTestimonialCard 
+                    text={el.text}
+                    userName={el.userName}
+                    userRole={el.userRole}
+               />
+                ))}
+                 </Slider>
+
+
+                </div> 
+               
+            </div>
+        </>
+    );
+}
+export default HomeTestimonial;
--- src/components/Pages/Services/Services.jsx
+++ src/components/Pages/Services/Services.jsx
@@ -1,5 +1,9 @@
 import style from './Services.module.css';
-import ServiceBOX from './ServicesBOX/HomeService'
+import ServiceBOX from './ServicesBOX/HomeService';
+import ServiceTestimonial from './ServiceTestimonial/ServiceTestimonial';
+import SerOne from '../../../img/ServiceICONS/Service Buisness.jpg';
+import SerTwo from '../../../img/ServiceICONS/Service Table.jpg';
+
 const Services = () =>{
     return (
         <>
@@ -18,6 +22,29 @@ const Services = () =>{
                 <div className={style.box}>
                   <ServiceBOX />
                 </div>
+            </div>
+            <div className={style.checkS}>
+                <div className={style.serviceContainer}>
+                    <div className={style.subServiceContainer}>
+                            <div className={style.subServiceContainerOne}>
+                                <div className={style.subServiceContainerOneHead}>WE PROVIDE YOU WITH THE BEST SERVICE IN THE MARKET</div>
+                                <div className={style.subServiceContainerOnePara}>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration, by injected humour. words</div>
+                            </div>
+                            <div className={style.subServiceContainerTwo}>
+                                <div className={style.subServiceContainerTwoPara}>There are many variations of passages of Lorem Ipsum available, but the majority have suffered alteration, Injected humour words web page editors now use as their.</div><br />
+                                <div className={style.subServiceContainerTwoPara}>Default and a search for will uncover many web sites still in their infancy.suffered alteration, by injected humour words web page.</div>
+                            </div>                            
+                         </div>
+                        <div className={style.serImgContainer}>
+                            <img src={SerOne} alt="Serices" heights={300} width={800} />
+                            <img src={SerTwo} alt="Serices" heights={300} width={355} />
+                        </div>
+
+                     </div>
+                     <div className={style.checkSer}> 
+                            <ServiceTestimonial />
+                     </div>
+
             </div>
 
             </div>
