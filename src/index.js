25d009f5915a7faf0300b04ce54658a3803ec466
Siddhant <me@siddhantgupta.in>
2024-03-15 13:47:03 +0530

    Updated the Scroll on Top in the Website

--- src/index.js
+++ src/index.js
@@ -3,12 +3,14 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import ScrollToTop from './components/ScrollToTop'; 
 
 import { BrowserRouter } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     < BrowserRouter>
+    <ScrollToTop />
          <App />
     </ BrowserRouter>
   </React.StrictMode>
