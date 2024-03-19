71ba4315fa72364020c227c37ff0d03508ded56c
Siddhant <me@siddhantgupta.in>
2024-03-19 05:45:08 +0530

    Updated the Hover Social Media Icons

--- src/components/Pages/AboutUs/OurTeam/OurTeam.jsx
+++ src/components/Pages/AboutUs/OurTeam/OurTeam.jsx
@@ -1,6 +1,8 @@
 import style from './OurTeam.module.css';
 
 import { FaCircle } from "react-icons/fa";
+import { AiFillInstagram } from "react-icons/ai";
+import { FaLinkedinIn } from "react-icons/fa";
 
 import Sparsh from "../../../../img/OurTeam/SHA02697.jpg";
 import Shailesh from "../../../../img/OurTeam/SHA02741.jpg";
@@ -28,6 +30,22 @@ const OurTeam =() =>{
                                 <div className={style.memberContent}>
                                     <div className={style.memberName}>Sparsh Agrawal</div>
                                     <div className={style.memberRole}> Founder </div>
+                                    <div className={style.userSocialMedia}>
+                                        <div className={style.userSocialMediaIcons}>
+                                            <a 
+                                               href="https://www.instagram.com/studio.knm/?igshid=MjEwN2IyYWYwYw%3D%3D" 
+                                               target="_blank" rel="noreferrer" >
+                                                <AiFillInstagram size={25} />
+                                            </a>
+                                        </div>
+                                        <div className={style.userSocialMediaIcons}>
+                                             <a 
+                                               href="https://www.linkedin.com/company/knm-studio/ "
+                                               target="_blank" rel="noreferrer" >
+                                              <FaLinkedinIn size={25} />
+                                             </a>
+                                        </div>
+                                    </div>
                                 </div>
                     </div>
                     <div className={style.member}>
@@ -35,6 +53,22 @@ const OurTeam =() =>{
                                 <div className={style.memberContent}>
                                     <div className={style.memberName}>Shailesh Tilekar</div>
                                     <div className={style.memberRole}> Creative Head </div>
+                                    <div className={style.userSocialMedia}>
+                                        <div className={style.userSocialMediaIcons}>
+                                            <a 
+                                               href="https://www.instagram.com/studio.knm/?igshid=MjEwN2IyYWYwYw%3D%3D" 
+                                               target="_blank" rel="noreferrer" >
+                                                <AiFillInstagram size={25} />
+                                            </a>
+                                        </div>
+                                        <div className={style.userSocialMediaIcons}>
+                                             <a 
+                                               href="https://www.linkedin.com/company/knm-studio/ "
+                                               target="_blank" rel="noreferrer" >
+                                              <FaLinkedinIn size={25} />
+                                             </a>
+                                        </div>
+                                    </div>
                                 </div>
                     </div>
                     <div className={style.member}>
@@ -42,6 +76,22 @@ const OurTeam =() =>{
                                 <div className={style.memberContent}>
                                     <div className={style.memberName}>Sayali RasKar</div>
                                     <div className={style.memberRole}> Chief Editor  </div>
+                                    <div className={style.userSocialMedia}>
+                                        <div className={style.userSocialMediaIcons}>
+                                            <a 
+                                               href="https://www.instagram.com/studio.knm/?igshid=MjEwN2IyYWYwYw%3D%3D" 
+                                               target="_blank" rel="noreferrer" >
+                                                <AiFillInstagram size={25} />
+                                            </a>
+                                        </div>
+                                        <div className={style.userSocialMediaIcons}>
+                                             <a 
+                                               href="https://www.linkedin.com/company/knm-studio/ "
+                                               target="_blank" rel="noreferrer" >
+                                              <FaLinkedinIn size={25} />
+                                             </a>
+                                        </div>
+                                    </div>
                                 </div>
                     </div>
                     <div className={style.member}>
@@ -49,6 +99,22 @@ const OurTeam =() =>{
                                 <div className={style.memberContent}>
                                     <div className={style.memberName}>Nikhil Inamke</div>
                                     <div className={style.memberRole}> Head of Videography </div>
+                                    <div className={style.userSocialMedia}>
+                                        <div className={style.userSocialMediaIcons}>
+                                            <a 
+                                               href="https://www.instagram.com/studio.knm/?igshid=MjEwN2IyYWYwYw%3D%3D" 
+                                               target="_blank" rel="noreferrer" >
+                                                <AiFillInstagram size={25} />
+                                            </a>
+                                        </div>
+                                        <div className={style.userSocialMediaIcons}>
+                                             <a 
+                                               href="https://www.linkedin.com/company/knm-studio/ "
+                                               target="_blank" rel="noreferrer" >
+                                              <FaLinkedinIn size={25} />
+                                             </a>
+                                        </div>
+                                    </div>
                                 </div>
                     </div>
 
