c187c9aa12542e6be5c40918326ebe403be24966
Siddhant <me@siddhantgupta.in>
2024-03-28 12:18:10 +0530

    Added the Content for Blog Two

---
+++ src/components/Pages/Blogs/BlogPages/BlogTwo.jsx
@@ -0,0 +1,74 @@
+import style from './BlogPages.module.css';
+
+const BlogTwo = () =>{
+    return (
+        <>
+            <div className={style.container}>
+                <div className={style.blogHead}>
+                    <div className={style.blogHeadingAdv}>Some of the Most Viral Advertisements</div>
+
+                </div>
+                <div className={style.blogContentContainer}>
+
+                 <div className={style.blogContent}>
+                            <div className={style.blogContentHead}>Introduction
+                            </div>    
+                             <p className={style.blogContentPara}>Advertising has come a long way from the days of print ads and billboards. In the digital age, advertisements have the power to go viral within seconds, reaching millions of viewers worldwide. These viral advertisements capture our attention, evoke emotions, and become an integral part of pop culture. In this blog, we'll explore some of the most viral advertisements that have left a lasting impact on both consumers and the advertising industry.</p>
+                             
+                             <div className={style.blogSubContent}>
+                                 <div className={style.blogSubContentHead}>1. Dove- Real Beauty Sketches (2013)
+                                 </div>   
+                                 <div className={style.blogSubContentPara}>One of the most impactful and viral ad campaigns of all time is Dove's "Real Beauty Sketches." This campaign aimed to challenge the way women perceive their own beauty. It featured an FBI-trained sketch artist drawing women based on their self-descriptions and then based on descriptions from others. The difference between the two sketches was astonishing and highlighted the importance of self-acceptance. This heartwarming and thought-provoking ad quickly went viral, promoting the message of self-esteem and self-love.
+                                 </div>  
+
+                                 <div className={style.blogSubContentHead}>2. Old Spice "The Man Your Man Could Smell Like" (2010)
+                                 </div>   
+                                 <div className={style.blogSubContentPara}>Old Spice redefined the way we view men's grooming products with their viral ad featuring "The Man Your Man Could Smell Like." This humorous and fast-paced commercial starred the charismatic Isaiah Mustafa, who humorously described the benefits of using Old Spice. The ad's catchy jingle, "I'm on a horse," became a cultural phenomenon. This advertisement showed that humor, creativity, and a memorable character can make an ad go viral.
+                                 </div>  
+                                 
+                                 <div className={style.blogSubContentHead}>3. Coca-Cola "Share a Coke" (2011)
+                                 </div>   
+                                 <div className={style.blogSubContentPara}>Coca-Cola's "Share a Coke" campaign transformed product personalization and social sharing. The campaign featured bottles and cans with popular names, encouraging people to share a Coke with their friends and loved ones. The idea was simple but highly effective. People started sharing images of their personalized Coke bottles on social media, using the hashtag #ShareACoke. This user-generated content and social media engagement helped the campaign go viral and boosted Coca-Cola's sales.
+                                 </div>  
+
+                                 <div className={style.blogSubContentHead}>4. Apple "1984"</div>   
+                                 <div className={style.blogSubContentPara}>Apple's "1984" advertisement, directed by Ridley Scott, is a historic moment in advertising. Aired during the 1984 Super Bowl, this ad introduced the Apple Macintosh and positioned it as a revolutionary product. The ad depicted a dystopian future and a woman smashing a giant screen, representing Apple's mission to challenge the status quo. With its powerful imagery and message, "1984" set a new standard for Super Bowl commercials and remains an iconic moment in advertising history.
+                                 </div>  
+
+                                 <div className={style.blogSubContentHead}>5. Budweiser "Puppy Love"
+                                 </div>   
+                                 <div className={style.blogSubContentPara}>Budweiser's "Puppy Love" ad is a heartwarming tale of the friendship between a Clydesdale horse and an adorable puppy. This ad masterfully combined humor and emotion, drawing viewers in with its endearing story. The commercial tugged at the heartstrings of millions, and its clever use of animals made it a social media sensation. This demonstrates how an emotional connection can help a brand's message resonate with the audience.
+
+                                 </div>  
+
+                                 <div className={style.blogSubContentHead}>6. Nike "Just Do It"
+                                 </div>   
+                                 <div className={style.blogSubContentPara}>Nike's "Just Do It" slogan has become a global icon. The brand's advertisements often feature athletes who have overcome adversity to achieve greatness. These ads inspire and motivate viewers, encouraging them to push their limits and pursue their dreams. The longevity and widespread recognition of "Just Do It" demonstrate the power of a simple, memorable slogan.
+                                 </div>  
+
+                                 <div className={style.blogSubContentHead}>7. Doritos "Crash the Super Bowl"
+                                 </div>   
+                                 <div className={style.blogSubContentPara}>Doritos took a creative approach to advertising by inviting fans to create their Super Bowl commercials. The "Crash the Super Bowl" campaign allowed people to submit their Doritos ads, with the chance to have them aired during the Super Bowl. This user-generated content not only engaged the audience but also generated a buzz around the brand. It's a prime example of how involving your audience can lead to viral success.
+                                 </div>  
+
+                            </div>    
+                            <div className={style.blogContentConclution}>Conclusion
+                            </div>    
+                             <p className={style.blogContentPara}>Viral advertisements are not just commercials; they are cultural phenomena that shape our perceptions, emotions, and behaviors. They make us laugh, cry, think, and share. Whether it's the emotional resonance of Budweiser's "Puppy Love" or the empowerment message of Nike's "Just Do It," viral ads have a lasting impact on our lives. These advertisements are not only memorable but also highly effective in achieving their marketing goals. In a digital age where attention spans are short, these ads prove that creativity, emotion, and user engagement are key to creating advertising campaigns that go beyond just selling products – they touch our hearts and minds.
+                             <br/>
+                             <br/>
+                             The power of viral advertising lies in its ability to connect with people on a personal level. It's about sparking conversations, creating trends, and inspiring change. As marketers continue to explore new avenues for reaching their target audiences, the lessons learned from these viral advertisements remain invaluable – a testament to the art and science of advertising.
+                             
+                             </p>
+                             <p className={style.blogContentPara}>So, next time you see a viral ad that moves you, makes you smile, or encourages you to share it with your friends, remember the creativity, storytelling, and emotional depth behind it, because that's what makes these advertisements truly extraordinary.
+                              <br />      <br />             
+                            For more exciting updates on viral ads stay tuned to our KNM blogs.</p>
+                             
+                </div>
+                </div>
+
+            </div>
+        </>
+    );
+}
+export default BlogTwo;
