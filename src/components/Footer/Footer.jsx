8dc5899294be0b43a4a851cb01d221178bf26db2
Siddhant <me@siddhantgupta.in>
2024-03-28 20:05:54 +0530

    Update the Responsive Interface in Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -9,16 +9,18 @@ const FOOTER = () =>{
                     <div className={style.callForAction}>
                          LET'S BUILD SOMETHING <br/> AMAZING TOGETHER
                     </div>
+                    <div className={style.subContainer}>
+
                     <div className={style.contact}>
                     <h3 className={style.footerHeading}>Contact</h3>
                          <span>T : +91 84596 39610</span><br />
                          <span>E : business@knmstudio.in</span> 
-
-                          
+      
                     </div>
                     <div className={style.address}>
                         <h3 className={style.footerHeading}>Head Office </h3>
                     101, Atheshri Apartment,<br />Baner, Pune, India <br />411045
+                    </div>
                     </div>
                 </div>
                 <div className={style.line} />
