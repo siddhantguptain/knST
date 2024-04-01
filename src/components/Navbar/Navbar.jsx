7a80c977468920ded3da8d270369db323771b1da
Siddhant <me@siddhantgupta.in>
2024-04-01 16:23:53 +0530

    Added the Phone responce -  480px for NavBar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,15 +2,15 @@ import React, {useState} from "react";
 import style from "./Navbar.module.css";
 
 import { Link } from "react-router-dom";
-// import CloseIcon from '@mui/icons-material/Close';
-import MenuIcon from '@mui/icons-material/Menu';
-
+import { IoClose } from "react-icons/io5";
+import { GiHamburgerMenu } from "react-icons/gi";
 import KnmLOGO from "../../img/KNM_LOGO.png";
 const NAVBAR = () =>{
-
+    const [navToggle , setNavToggle] = useState(false);
 
     // Bg-Color
     const [bgColor , setBgColor] = useState(false);
+     
     const changeBgColor = ()=>{
         if (window.scrollY  >= 90) {
             setBgColor(true);
@@ -22,8 +22,8 @@ const NAVBAR = () =>{
 
     return (
         <>
-           <header className={bgColor ? `${style.headerBg}` : ''}>
-            <div className={style.navbar} >
+           <header className={bgColor ? `${style.headerBg}` : ''} >
+            <div className={navToggle ? `${style.headerBg} ${style.navbar}` : `${style.navbar}`} >
                 <div className={style.logo}>
                         <img src={KnmLOGO} alt="knmStudio" height={90}  />
                 </div>
@@ -40,23 +40,24 @@ const NAVBAR = () =>{
                      Start a Project
                    </div>
                 </Link>
-                <div className={style.toggleNav}>
-                      {/* <CloseIcon />  */}
-                      <MenuIcon />
+                <div className={style.toggleNav} onClick={() => setNavToggle(!navToggle)}>
+                    {!navToggle ? <GiHamburgerMenu size={27} />:<IoClose size={27} />}
                 </div>
             </div>   
                 {/* DropDown */}
+                <div className={!navToggle ? `${style.dropDownInActive}` : `${style.dropDownActive}`} onClick={() => setNavToggle(!navToggle)}>
                 <div className={style.dropDown}>
-                    <li className={style.link}>Home</li>
-                    <li className={style.link}>About Us</li>
-                    <li className={style.link}>Services</li>
-                    <li className={style.link}>Our Work</li>
-                    <li className={style.link}>Blogs</li>
-                    <li className={style.link}>Contact Us</li>
+                <li className={style.link}><Link to="/">Home</Link></li>
+                    <li className={style.link}><Link to="/about-us">About Us</Link></li>
+                    <li className={style.link}><Link to="/services">Services</Link></li>
+                    <li className={style.link}><Link to="/our-work">Our Work</Link></li>
+                    <li className={style.link}><Link to="/blogs">Blogs</Link></li>
+                    <li className={style.link}><Link to="/contact-us">Contact Us</Link></li>
                     <li className={style.link}>  <div className={style.actionBtn}>
                     Start a Project
                      </div></li>
                 </div>
+            </div>
            </header>
         </>
     );
