6a9531a23a0bc983b82199685ce75e3b3629fcc8
Siddhant <me@siddhantgupta.in>
2024-04-02 01:47:15 +0530

    Added the Blog Home Page Along with Card Component

---
+++ src/components/Pages/Blogs/BlogPageCard/BlogPageCard.jsx
@@ -0,0 +1,26 @@
+import style from './BlogPageCard.module.css';
+import KnmButton from "../../../KnmButton/KnmButton";
+
+const BlogPageCard =(props) =>{
+    return(
+        <>  
+            <div className={style.container}>
+                <div className={style.imgDIVContainer}>
+                    <span className={style.imgDIV}>
+                         {props.src}
+                    </span>
+                </div>
+                <div className={style.sub}>
+                     <div className={style.tag}>{props.tag}</div>
+                     <div className={style.title}>{props.title}</div>
+                     <div className={style.para}>{props.para}</div>
+                     <div className={style.readBtn}>
+                            <KnmButton text="Read More." />
+
+                     </div>
+                </div>    
+            </div>
+        </>
+    );
+}
+export default BlogPageCard;
--- src/components/Pages/Blogs/Blogs.jsx
+++ src/components/Pages/Blogs/Blogs.jsx
@@ -1,14 +1,39 @@
 import style from './Blogs.module.css';
-import BlogOne from './BlogPages/BlogOne';
-import BlogTwo from './BlogPages/BlogTwo';
-import BlogThree from './BlogPages/BlogThree';
+// import BlogOne from './BlogPages/BlogOne';
+// import BlogTwo from './BlogPages/BlogTwo';
+// import BlogThree from './BlogPages/BlogThree';
+
+import BlogPageCard from "./BlogPageCard/BlogPageCard";
+import BL1 from "../../../img/BlogImg/Some-of-the-Most-Viral-Advertisements.jpg"
+import BL2 from "../../../img/BlogImg/The-Importance-of-Visual-Content-Creation-in-Large-Companies-and-Businesses-thumbnail.jpg"
+import BL3 from "../../../img/BlogImg/BLOG 3 Influencer.png"
 const Blogs = () =>{
     return (
         <>
             <div className={style.container}>
-               <BlogOne />
-               <BlogTwo />
-               <BlogThree />
+                    <BlogPageCard 
+                        src={   <img src={BL1} alt=">Why is marketing important for businesses?" height="300"/>} 
+                        tag="July 12, 2023 .. Marketing"
+                        title="Why is marketing important for businesses?"
+                        para="It's unfortunate when good businesses don't focus on marketing. They     have      excellent products,
+                        but because they're not visible, potential customers can't find them. This leads to lost sales as
+                     customers choose their competitors instead. Business owners feel frustrated knowing their
+                     products could make a difference if more people were aware of them..."
+                    />
+                    <BlogPageCard 
+                        src={   <img src={BL2} alt="The Importance of
+                        Visual Content Creation." height="300"/>} 
+                        tag="July 18, 2023 .. Editorial"
+                        title="The Importance of
+                        Visual Content Creation."
+                        para="In an era dominated by digital innovation and information overload, businesses, both large and small, find themselves in a constant battle for online visibility and customer engagement. In this relentless pursuit of digital success, visual content creation has emerged as a potent weapon. The importance of visual content in the marketing strategies..."
+                    />
+                    <BlogPageCard 
+                        src={   <img src={BL3} alt=">Some of the Most Viral Advertisements" height="300"/>} 
+                        tag="July 22, 2022 .. Educational"
+                        title="Some of the Most Viral Advertisements."
+                        para="Advertising has come a long way from the days of print ads and billboards. In the digital age, advertisements have the power to go viral within seconds, reaching millions of viewers worldwide. These viral advertisements capture our attention, evoke emotions, and become an integral part of pop culture. In this blog, we'll explore ..."
+                    />
             </div>
         </>
     );
