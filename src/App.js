060aafe48c3419f111c77a20a027cced382c2762
Siddhant <me@siddhantgupta.in>
2024-04-02 15:15:24 +0530

    Routed the Blog to There individual Pages

--- src/App.js
+++ src/App.js
@@ -6,6 +6,9 @@ import AboutUs from './components/Pages/AboutUs/AboutUs';
 import Services from './components/Pages/Services/Services';
 import OurWork from './components/Pages/OurWork/OurWork';
 import Blogs from './components/Pages/Blogs/Blogs';
+import BlogOne from './components/Pages/Blogs/BlogPages/BlogOne';
+import BlogTwo from './components/Pages/Blogs/BlogPages/BlogTwo';
+import BlogThree from './components/Pages/Blogs/BlogPages/BlogThree';
 import ContactUs from './components/Pages/ContactUs/ContactUs';
 import SocialMedia from './components/SocialMedia/SocialMedia';
 import FOOTER from './components/Footer/Footer';
@@ -16,11 +19,18 @@ function App() {
            <NAVBAR />
             <Routes>
               <Route path="/" element={<Home />} />
-              {/* <Route path="/about-us" element={<AboutUs />}  />
+              <Route path="/about-us" element={<AboutUs />}  />
               <Route path="/services" element={<Services />}  />
               <Route path="/our-work" element={<OurWork />}  />
-              <Route path="/blogs" element={<Blogs />}  />
-              <Route path="/contact-us" element={<ContactUs />}  /> */}
+              <Route path="/blogs" element={
+                  <>  
+                        <Blogs />
+                  </>
+              }  />
+              <Route path="/blogs/The-Importance-of-Visual-Content" element={<BlogOne />} />
+              <Route path="/blogs/Some-of-the-Most-Viral-Advertisements" element={<BlogTwo />}  />
+              <Route path="/blogs/Why-is-Marketing-Important-for-Business?" element={<BlogThree />}  />
+              <Route path="/contact-us" element={<ContactUs />}  /> 
             </Routes>
           <SocialMedia />
           <FOOTER />
