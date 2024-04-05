0300d1b8ce5af19b0a9e2c524c411043724ebb9f
Siddhant <me@siddhantgupta.in>
2024-04-05 11:42:52 +0530

    Added the Blog Imgs

--- src/components/Pages/Blogs/BlogPages/BlogOne.jsx
+++ src/components/Pages/Blogs/BlogPages/BlogOne.jsx
@@ -1,5 +1,16 @@
 import style from './BlogPages.module.css';
 
+import BrandIdentity from "../../../../img/BlogImg/Brand Identity.png"
+import DemonstratingExpertise from "../../../../img/BlogImg/Demonstrating-expertise.png"
+import EnhancedUserEngagement from "../../../../img/BlogImg/Enhanced-User-Engagement.png"
+import GoingViral from "../../../../img/BlogImg/Going-viral.png"
+import IncreasedConversion from "../../../../img/BlogImg/Increased-Conversion.png"
+import InternationalAppeal from "../../../../img/BlogImg/International-Appeal.png"
+import MultifacetedApproach from "../../../../img/BlogImg/Multifaceted-approach.png"
+import SEOBenefits from "../../../../img/BlogImg/SEO-Benefits.png"
+import SocialMediaDominance from "../../../../img/BlogImg/Social-media-dominance.png"
+import ThePowerOfStorytelling from "../../../../img/BlogImg/The-Power-of-Storytelling.png"
+
 const BlogOne = () =>{
     return (
         <>
@@ -17,52 +28,91 @@ const BlogOne = () =>{
                              
                              <div className={style.blogSubContent}>
                                  <div className={style.blogSubContentHead}>1. Visual Content: A Gateway to Brand Identity
-                                 </div>   
+                                 </div>  
+                                 <div className={style.blogImg}> 
+                                 <img src={BrandIdentity} alt="The Importance of  Visual Content 
+                                 Creation." height="200"/>
+                                 </div>
                                  <div className={style.blogSubContentPara}>Visual content is a direct representation of your brand's corporate identity. The images, videos, infographics, and other visual elements you produce should convey the essence of your company and its core values. In the case of large companies, the brand identity is often well-established. Visual content reinforces and amplifies this identity, making it easier for consumers to recognize and connect with the brand.
                                  </div>  
 
                                  <div className={style.blogSubContentHead}>2. The Power of Storytelling
-                                 </div>   
+                                 </div>  
+                                 <div className={style.blogImg}> 
+                                 <img src={ThePowerOfStorytelling} alt="The Importance of  Visual Content 
+                                 Creation." height="300"/>
+                                 </div> 
                                  <div className={style.blogSubContentPara}>Visual content is a compelling tool for brand storytelling. Through engaging visuals, businesses can narrate their story, showcase their journey, and highlight their mission and vision. These stories resonate with consumers on a deeper level, fostering loyalty and trust. Visual content can encapsulate the history, culture, and values of a company in a way that words alone cannot achieve.
                                  </div>  
                                  
                                  <div className={style.blogSubContentHead}>3. A Multifaceted Approach
-                                 </div>   
+                                 </div>  
+                                 <div className={style.blogImg}> 
+                                 <img src={MultifacetedApproach} alt="The Importance of  Visual Content 
+                                 Creation." height="300"/>
+                                 </div>  
                                  <div className={style.blogSubContentPara}>In the digital age, consumers have diverse preferences. Some prefer to read, while others are more inclined towards visual content. Large companies must cater to both these segments by employing a multifaceted content strategy. Visual content, such as explainer videos, infographics, and product demonstrations, complements textual content by offering an alternative and often more engaging way to convey information.
                                  </div>  
 
-                                 <div className={style.blogSubContentHead}>4. Enhanced User Engagement                                 </div>   
+                                 <div className={style.blogSubContentHead}>4. Enhanced User Engagement                                 </div>  
+                                 <div className={style.blogImg}> 
+                                 <img src={EnhancedUserEngagement} alt="The Importance of  Visual Content 
+                                 Creation." height="300"/>
+                                 </div>  
                                  <div className={style.blogSubContentPara}>One of the most significant advantages of visual content is its capacity to enhance online user engagement. According to a study, content containing relevant images or videos receives 94% more views than content without. This higher engagement rate can translate into longer time spent on your website, increased shares on social media, and improved search engine rankings
                                  </div>  
 
                                  <div className={style.blogSubContentHead}>5. SEO Benefits
-                                 </div>   
+                                 </div>  
+                                 <div className={style.blogImg}> 
+                                 <img src={SEOBenefits} alt="The Importance of  Visual Content 
+                                 Creation." height="300"/>
+                                 </div>  
                                  <div className={style.blogSubContentPara}>Speaking of SEO, the role of visual content in improving search engine rankings should not be underestimated. By optimizing images and videos with descriptive alt text and captions, you can make your content more discoverable by search engines. Moreover, as search engines increasingly favour multimedia content, adding images and videos to your pages can positively impact your digital marketing and SEO efforts.
                                  </div>  
 
                                  <div className={style.blogSubContentHead}>6. Going Viral
-                                 </div>   
+                                 </div>  
+                                 <div className={style.blogImg}> 
+                                 <img src={GoingViral} alt="The Importance of  Visual Content 
+                                 Creation." height="300"/>
+                                 </div>  
                                  <div className={style.blogSubContentPara}>The dream of every marketer is to create content that goes viral. Visual content, due to its shareable nature and tendency to evoke emotion, is more likely to be shared across social media platforms. A single viral video can expose a brand to millions of potential customers, making it an invaluable asset for large companies seeking to expand their reach.
                                  </div>  
 
                                  <div className={style.blogSubContentHead}>7. Increased Conversions
-                                 </div>   
+                                 </div>
+                                 <div className={style.blogImg}> 
+                                 <img src={IncreasedConversion} alt="The Importance of  Visual Content 
+                                 Creation." height="300"/>
+                                 </div>    
                                  <div className={style.blogSubContentPara}>Visual content doesn't just capture attention; it also drives conversion rates. Studies have shown that adding a product video on a landing page can increase conversions by 80% or more. Customers feel more confident in their purchase decisions when they can see a product in action or understand a service through visuals.
                                  </div>  
 
                                  <div className={style.blogSubContentHead}>8. Social Media Dominance
-
                                  </div>   
+                                 <div className={style.blogImg}> 
+                                 <img src={SocialMediaDominance} alt="The Importance of  Visual Content 
+                                 Creation." height="300"/>
+                                 </div> 
                                  <div className={style.blogSubContentPara}>ILarge companies often maintain a strong presence on various social media platforms. Visual content is tailor-made for these platforms, where scrolling users make quick judgments based on the appeal of the visuals. High-quality images, well-crafted videos, and eye-catching infographics are essential tools for social media success.
                                  </div>  
 
                                  <div className={style.blogSubContentHead}>9. Demonstrating Expertise
-                                 </div>   
+                                 </div>  
+                                 <div className={style.blogImg}> 
+                                 <img src={DemonstratingExpertise} alt="The Importance of  Visual Content 
+                                 Creation." height="300"/>
+                                 </div>  
                                  <div className={style.blogSubContentPara}>Visual content allows businesses to showcase their industry expertise. By creating informative and visually appealing content, companies can position themselves as thought leaders, attracting a loyal following and potential partnerships.
                                  </div>  
 
                                  <div className={style.blogSubContentHead}>10. International Appeal
                                  </div>   
+                                 <div className={style.blogImg}> 
+                                 <img src={InternationalAppeal} alt="The Importance of  Visual Content 
+                                 Creation." height="300"/>
+                                 </div> 
                                  <div className={style.blogSubContentPara}>For big companies with a global reach, visual content transcends language barriers. A compelling image or video can convey the essence of a brand and its offerings, making it universally understandable and appealing to diverse audiences.
                                  </div>  
                                  
