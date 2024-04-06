1108526889fb7d7405365ac685a464e2e03144e8
Siddhant <me@siddhantgupta.in>
2024-04-06 13:04:48 +0530

    Added the Img in the Blog Two

--- src/components/Pages/Blogs/BlogPages/BlogTwo.jsx
+++ src/components/Pages/Blogs/BlogPages/BlogTwo.jsx
@@ -1,4 +1,11 @@
 import style from './BlogPages.module.css';
+import appleThumbnail from "../../../../img/BlogImg/apple-thumbnail.jpg"
+import budweiserThumbnail from "../../../../img/BlogImg/budweiser-thumbnail.jpg"
+import CocaColaThumbnail from "../../../../img/BlogImg/Coca-cola-thumbnail.jpg"
+import DoritosThumbnail from "../../../../img/BlogImg/Doritos-thumbnail.jpg"
+import DoveThumbnail from "../../../../img/BlogImg/Dove-thumbnail.jpg"
+import nikeThumbnail from "../../../../img/BlogImg/nike-thumbnail.jpg"
+import OldSpiceThumbnail from "../../../../img/BlogImg/Old-spice-thumbnail.jpg"
 
 const BlogTwo = () =>{
     return (
@@ -18,36 +25,64 @@ const BlogTwo = () =>{
                              <div className={style.blogSubContent}>
                                  <div className={style.blogSubContentHead}>1. Dove- Real Beauty Sketches (2013)
                                  </div>   
+                                 <div className={style.blogImg}> 
+                                 <img src={DoveThumbnail} alt="The Importance of  Visual Content 
+                                 Creation." height="300"/>
+                                 </div>
                                  <div className={style.blogSubContentPara}>One of the most impactful and viral ad campaigns of all time is Dove's "Real Beauty Sketches." This campaign aimed to challenge the way women perceive their own beauty. It featured an FBI-trained sketch artist drawing women based on their self-descriptions and then based on descriptions from others. The difference between the two sketches was astonishing and highlighted the importance of self-acceptance. This heartwarming and thought-provoking ad quickly went viral, promoting the message of self-esteem and self-love.
                                  </div>  
 
                                  <div className={style.blogSubContentHead}>2. Old Spice "The Man Your Man Could Smell Like" (2010)
-                                 </div>   
+                                 </div>  
+                                 <div className={style.blogImg}> 
+                                 <img src={OldSpiceThumbnail} alt="The Importance of  Visual Content 
+                                 Creation." height="300"/>
+                                 </div> 
                                  <div className={style.blogSubContentPara}>Old Spice redefined the way we view men's grooming products with their viral ad featuring "The Man Your Man Could Smell Like." This humorous and fast-paced commercial starred the charismatic Isaiah Mustafa, who humorously described the benefits of using Old Spice. The ad's catchy jingle, "I'm on a horse," became a cultural phenomenon. This advertisement showed that humor, creativity, and a memorable character can make an ad go viral.
                                  </div>  
                                  
                                  <div className={style.blogSubContentHead}>3. Coca-Cola "Share a Coke" (2011)
+                                 </div> 
+                                 <div className={style.blogImg}> 
+                                 <img src={CocaColaThumbnail} alt="The Importance of  Visual Content 
+                                 Creation." height="300"/>
                                  </div>   
                                  <div className={style.blogSubContentPara}>Coca-Cola's "Share a Coke" campaign transformed product personalization and social sharing. The campaign featured bottles and cans with popular names, encouraging people to share a Coke with their friends and loved ones. The idea was simple but highly effective. People started sharing images of their personalized Coke bottles on social media, using the hashtag #ShareACoke. This user-generated content and social media engagement helped the campaign go viral and boosted Coca-Cola's sales.
                                  </div>  
 
-                                 <div className={style.blogSubContentHead}>4. Apple "1984"</div>   
+                                 <div className={style.blogSubContentHead}>4. Apple "1984"</div>  
+                                 <div className={style.blogImg}> 
+                                 <img src={appleThumbnail} alt="The Importance of  Visual Content 
+                                 Creation." height="300"/>
+                                 </div>  
                                  <div className={style.blogSubContentPara}>Apple's "1984" advertisement, directed by Ridley Scott, is a historic moment in advertising. Aired during the 1984 Super Bowl, this ad introduced the Apple Macintosh and positioned it as a revolutionary product. The ad depicted a dystopian future and a woman smashing a giant screen, representing Apple's mission to challenge the status quo. With its powerful imagery and message, "1984" set a new standard for Super Bowl commercials and remains an iconic moment in advertising history.
                                  </div>  
 
                                  <div className={style.blogSubContentHead}>5. Budweiser "Puppy Love"
                                  </div>   
+                                 <div className={style.blogImg}> 
+                                 <img src={budweiserThumbnail} alt="The Importance of  Visual Content 
+                                 Creation." height="300"/>
+                                 </div> 
                                  <div className={style.blogSubContentPara}>Budweiser's "Puppy Love" ad is a heartwarming tale of the friendship between a Clydesdale horse and an adorable puppy. This ad masterfully combined humor and emotion, drawing viewers in with its endearing story. The commercial tugged at the heartstrings of millions, and its clever use of animals made it a social media sensation. This demonstrates how an emotional connection can help a brand's message resonate with the audience.
 
                                  </div>  
 
                                  <div className={style.blogSubContentHead}>6. Nike "Just Do It"
-                                 </div>   
+                                 </div>  
+                                 <div className={style.blogImg}> 
+                                 <img src={nikeThumbnail} alt="The Importance of  Visual Content 
+                                 Creation." height="300"/>
+                                 </div>  
                                  <div className={style.blogSubContentPara}>Nike's "Just Do It" slogan has become a global icon. The brand's advertisements often feature athletes who have overcome adversity to achieve greatness. These ads inspire and motivate viewers, encouraging them to push their limits and pursue their dreams. The longevity and widespread recognition of "Just Do It" demonstrate the power of a simple, memorable slogan.
                                  </div>  
 
                                  <div className={style.blogSubContentHead}>7. Doritos "Crash the Super Bowl"
-                                 </div>   
+                                 </div>  
+                                 <div className={style.blogImg}> 
+                                 <img src={DoritosThumbnail} alt="The Importance of  Visual Content 
+                                 Creation." height="300"/>
+                                 </div>  
                                  <div className={style.blogSubContentPara}>Doritos took a creative approach to advertising by inviting fans to create their Super Bowl commercials. The "Crash the Super Bowl" campaign allowed people to submit their Doritos ads, with the chance to have them aired during the Super Bowl. This user-generated content not only engaged the audience but also generated a buzz around the brand. It's a prime example of how involving your audience can lead to viral success.
                                  </div>  
 
